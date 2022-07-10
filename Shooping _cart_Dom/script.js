3b72eae9db0c1aed55377e6551eac9c742249bc9
Gasmi_Ghassen <89700144+gasmighassen@users.noreply.github.com>
2022-07-10 23:10:51 +0100

    final script

--- Shooping _cart_Dom/script.js
+++ Shooping _cart_Dom/script.js
@@ -6,6 +6,7 @@ let price = document.getElementsByClassName('price');
 let btnDelete = document.getElementsByClassName('delete');
 let items = document.getElementsByClassName('Item');
 
+
 /* function to change the heart shape button to red onclick*/
 
 for(let i = 0 ; i < hearts.length ; i++)
@@ -21,32 +22,54 @@ for(let i = 0 ; i < hearts.length ; i++)
     });
 }
 
-/*fonction to decrement item quantity*/ 
+/*fonction to decrement item quantity with minus nutton*/ 
 
     for(let i = 0 ; i < btnMinus.length ; i++){
         btnMinus[i].addEventListener('click', function onClick() {
-            if(input[i].value > 0){
+            if(input[i].value >= 1){
                 input[i].value--;
             }       
-            
+            TotalPrice() 
         });
        
     }
-/* function to increment item quantity*/
+
+/* function to increment item quantity with plus*/
 
     for(let i = 0 ; i < btnPlus.length ; i++){
         btnPlus[i].addEventListener('click', function onClick() {
             input[i].value++; 
-            
+            TotalPrice() 
         });
         
     }
    
 /* function to delete item from cart*/
 
-        for(let i = 0 ; i < btnDelete.length ; i++){
-            btnDelete[i].addEventListener('click', function onClick() {
-                items[i].remove();
-               i--;
-            });
+        for( var i =0 ; i < btnDelete.length ; i++) 
+        {
+            var button = btnDelete[i]
+            button.addEventListener('click', function(event){
+                var buttonclicked = event.target
+                buttonclicked.parentElement.remove ()
+                TotalPrice() 
+            })
+        }                
+
+/*function to calculate the total price*/     
+
+        function TotalPrice() 
+        {
+            var ItemContainer = document.getElementsByClassName('items')[0]
+            var cartItems = document.getElementsByClassName('Item')
+            var total = 0
+            for( var i =0; i< cartItems.length ; i++){
+                 var cartItem = cartItems [i]
+                 var priceElement = cartItem.getElementsByClassName('price')[0]
+                 var quantityElement = cartItem.getElementsByClassName('Quant')[0]
+                 var price = parseFloat (priceElement.innerText)
+                 var quantity = quantityElement.value
+                 total = total+( price * quantity)
+             }
+            document.getElementById('finalPrice').value = total
         }
